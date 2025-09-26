0fafe96bb1decfa75dec5dfc1efd176cf3016474
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2025-09-26 16:17:17 +0200

    fix: insights sharing

--- components/InsightsPage/Insights.tsx
+++ components/InsightsPage/Insights.tsx
@@ -1,17 +1,37 @@
-'use client'
+'use client';
+
 import InsightModal from "@/components/insight-modal";
-import { fetchInsightById,  } from "@/lib/api";
+import { fetchInsightById } from "@/lib/api";
+import { useState, useEffect } from "react";
+import LoadingSpinner from "../LoadingSpinner";
+import { useRouter } from "next/navigation";
 
+interface InsightPageProps {
+  id: string;
+}
 
-export default async function InsightPage({ id }: { id: string }) {
-  const insight = await fetchInsightById(id);
-  return <InsightModal
-  insight={insight}
-  isOpen={true}
-  onClose={() => {
-    window.history.pushState({}, "", "/insights");
-    // setInsight(null);
-  }}
-/>
-;
+export default function InsightPage({ id }: InsightPageProps) {
+  const [insight, setInsight] = useState<any>(null);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+const router = useRouter();
+  useEffect(() => {
+    // fetch insight data on mount
+    fetchInsightById(id).then((data) => {
+      console.log("Fetched insight:", data); 
+      setInsight(data);
+    });
+  }, [id]);
+
+  if (!insight) return <LoadingSpinner />; 
+
+  return (
+    <InsightModal
+      insight={insight}
+      isOpen={isModalOpen}
+      onClose={() => {
+          setIsModalOpen(false); 
+        router.push("/insights"); 
+      }}
+    />
+  );
 }
